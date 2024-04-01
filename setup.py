9e8d58f95eebe5faa328f3b3cf794df5b020f71b
brunoboivin <bruno.boivin@mail.mcgill.ca>
2024-04-01 16:13:46 -0700

    IT-1137 add missing maintainers info

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup_kwargs = {
     'long_description': '# isx: pure-python API to read Inscopix data\n\n![](https://github.com/inscopix/py_isx/actions/workflows/main.yml/badge.svg) \n![](https://img.shields.io/pypi/v/isx)\n\nThis is a pure-python API to read Inscopix ISXD files. \n\n\n## Documentation\n\n[Read the documentation](https://inscopix.github.io/py_isx/)\n\n## Support\n\n|  File type | Support |\n|  --------- | ------- |\n| ISXD CellSet   | ✅ |\n| ISXD Movie   | ✅ |\n| ISXD Movie (multi-plane)   | ❌ |\n| ISXD Movie (dual-color)   | ❌ |\n| GPIO data   | ❌ |\n| ISXD Events   | ❌ |\n| ISXD VesselSet   | ❌ |\n\n\n## Install\n\n### Poetry\n\n```bash\npoetry add isx\n```\n\n### pip\n\n\n```bash\npip install isx\n```\n\n## Caution\n\nThis is a work in progress, and all reading functions in the IDPS Python API are not supported yet. \n\n\n## Testing\n\nThis code is tested using GitHub Actions on the following python\nversions:\n\n- 3.9\n- 3.10\n- 3.11\n- 3.12\n',
     'author': 'Inscopix, Inc.',
     'author_email': 'support@inscopix.com',
+    'maintainer': 'Inscopix, Inc.',
+    'maintainer_email': 'support@inscopix.com',
     'url': 'https://github.com/inscopix/py_isx',
     'packages': packages,
     'package_data': package_data,

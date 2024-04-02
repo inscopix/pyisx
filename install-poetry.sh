e00cecabed7ce967729891187dff9cd60189a0a4
Bruno Boivin <bruno.boivin@mail.mcgill.ca>
2024-04-02 13:32:53 -0700

    IT-1137: enforce movie data type (#52)
    * IT-1137 generalize author/email info
    
    * IT-1137 ignore IDE config files
    
    * IT-1137 fix movie data type inconsistency
    
    * IT-1137 add missing maintainers info
    
    * fixes for testing
    
    ---------
    
    Co-authored-by: Nosheen Adil <nadil@inscopix.com>

--- install-poetry.sh
+++ install-poetry.sh
@@ -3,7 +3,5 @@ then
     echo "poetry could not be found, installing..."
     curl -sSL https://install.python-poetry.org | python3 -
     echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bash_profile
-    # tell git to use SSH to get code
-    git config --global url.ssh://git@github.com/.insteadOf https://github.com/
     exit
 fi

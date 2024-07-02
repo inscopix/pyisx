c7b66849e09a0c7cdb4a6d4472f4f056c2b18ac6
Nosheen Adil <nadil@inscopix.com>
2024-07-02 11:10:56 -0700

    remove pure python files

--- install-poetry.sh
+++
@@ -1,7 +0,0 @@
-if ! command -v poetry &> /dev/null
-then
-    echo "poetry could not be found, installing..."
-    curl -sSL https://install.python-poetry.org | python3 -
-    echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bash_profile
-    exit
-fi
